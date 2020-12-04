f51c4bf227281b0ab8eadf71935fa2b597e1fa6a
IULIA-MARIA FLOREA <iuliaxflorea@yahoo.com>
2020-12-04 17:35:54 +0200

    Laborator 9

--- Florea_Iulia_Lab8/Models/Book.cs
+++ Florea_Iulia_Lab8/Models/Book.cs
@@ -4,16 +4,46 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Policy;
 
 namespace Florea_Iulia_Lab8.Models
 {
     public class Book
     {
         public int ID { get; set; }
+        [Required, StringLength(150, MinimumLength = 3)]
+
 
         [Display(Name = "Book Title")]
         public string Title { get; set; }
+        [RegularExpression(@"^[A-Z][a-z]+\s[A-Z][a-z]+$", ErrorMessage = "Numele autorului trebuie sa fie de forma 'Prenume Nume'"), Required, StringLength(50, MinimumLength = 3)]
+        //^ marcheaza inceputul sirului de caractere
+        //[A-Z][a-z]+ prenumele -litera mare urmata de oricate litere mici
+        //\s spatiu
+        //[A-Z][a-z]+ numele- litera mare urmata de oricate litere mici
+        //$ marcheaza sfarsitul sirului de caractere
         public string Author { get; set; }
+        [Range(1, 300)]
+
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
+
+        public int PublisherID { get; set; }
+        public Publisher Publisher { get; set; }
+        public ICollection<BookCategory> BookCategories { get; set; }
+    }
+}
+
+//^ marcheaza inceputul sirului de caractere
+//[A-Z][a-z]+ prenumele -litera mare urmata de oricate litere mici
+//\s spatiu
+//[A-Z][a-z]+ numele- litera mare urmata de oricate litere mici
+//$ marcheaza sfarsitul sirului de caractere
+/*public string Author { get; set; }
+
+         [Range(1, 300)]
 
         [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
@@ -27,4 +57,4 @@ namespace Florea_Iulia_Lab8.Models
         public ICollection<BookCategory> BookCategories { get; set; }
 
     }//navigation property
-}
+}*/
